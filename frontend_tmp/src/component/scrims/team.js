46e1a280b36b5675ed0f2a7b7bd25282fe6c5f1f
FranMori <francois.moriame@gmail.com>
2022-02-22 11:23:56 +0100

    test infinte loops

--- frontend_tmp/src/component/scrims/team.js
+++ frontend_tmp/src/component/scrims/team.js
@@ -1,6 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Button from 'react-bootstrap/Button'
 import axios from 'axios'
+import { Component, useState } from 'react'
+
 
 import {Link, Outlet} from 'react-router-dom'
 
@@ -17,44 +19,70 @@ const getTopPrepaOtf2 = 'http://localhost:5000/api/matchups/top/getAllPrepaOtf2'
 
 function Team () {
 
+const [countGames, setCountGames] = useState()
+
+
 let topPicks = []
+
 axios.get(getTopScrim).then(res => {
 
 for (let i = 0; i < res.data[0].length; i++) {
   topPicks.push(res.data[0][i].EPick1)
 }
+
+axios.get(getTopOtf1).then(res => {
+  for(let i = 0; i < res.data[0].length; i++) {
+    topPicks.push(res.data[0][i].EPick1)
+  }
+
+axios.get(getTopPrepaOtf2).then(res => {
+  console.log(res.data)
+  let test = []
+  test.push(res.data)
+  console.log(test)
+  for(let i = 0; i < res.data[0].length; i++) {
+    topPicks.push(res.data[0][i].EPick1)
+  }
+  console.log(topPicks)
+
+  const count = {};
+
+  for (const element of topPicks) {
+    if (count[element]) {
+      count[element] += 1;
+    } else {
+      count[element] = 1;
+    }
+  }
+// setCountGames(count)
 })
-// axios.get(getTopOtf1).then(res => {
-//   for(let i = 0; i < res.data[0].length; i++) {
-//     topPicks.push(res.data[0][i].EPick1)
-//   }
-// })
-// axios.get(getTopPrepaOtf2).then(res => {
-//   for(let i = 0; i < res.data[0].length; i++) {
-//     topPicks.push(res.data[0][i].EPick1)
-//   }
-// })
+})
+})
 
-function getOccurrence(array, value) {
-  return array.filter((v) => (v === value)).length;
-}
-console.log(topPicks)
 
-let ornnScrimGames = getOccurrence(topPicks, 'Ornn')
-let camilleScrimGames = getOccurrence(topPicks, 'Camille')
-let jayceScrimGames = getOccurrence(topPicks, 'Jayce')
-let shenScrimgames = getOccurrence(topPicks, 'Shen')
-let kennenScrimGames = getOccurrence(topPicks, 'Kennen')
-let renektonScrimGames = getOccurrence(topPicks, 'Renekton')
-let ireliaScrimGames = getOccurrence(topPicks, 'Irelia')
-let akaliScrimGames = getOccurrence(topPicks, 'Akali')
-let gnarGames = getOccurrence(topPicks, 'Gnar')
-
-console.log(ornnScrimGames)
 
   return(
    <div>
      <Header />
+     {/* <table>
+       <tbody>
+         <tr>
+           <td>Akali</td>
+           <td>{countGames.Akali} Games</td>
+           <td>%win</td>
+         </tr>
+         <tr>
+           <td>Ornn</td>
+           <td>{countGames.Ornn} Games</td>
+           <td>%win</td>
+         </tr>
+         <tr>
+           <td>Ornn</td>
+           <td>{countGames.Ornn} Games</td>
+           <td>%win</td>
+         </tr>
+       </tbody>
+     </table> */}
      <div className='navOTF'>
         <Button variant="warning"><Link to = "/team/prepaotf1">Prépa OTF1</Link></Button>
         <Button variant="warning"><Link to = "/team/otf1">OTF1</Link></Button>
