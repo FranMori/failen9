c25f76a1b56350459e8bc2538960e5c0d137d5d0
FranMori <francois.moriame@gmail.com>
2022-03-01 11:03:20 +0100

    commit

--- backend/utils/database.js
+++ backend/utils/database.js
@@ -1,7 +1,8 @@
+
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize ('F9', 'root', 'bouftou80', {
-  host: 'localhost',
+const sequelize = new Sequelize ('heroku_f15694e4d3d9e2c', 'b61a4949d98783', 'eb75d6cd', {
+  host: 'us-cdbr-east-05.cleardb.net',
   dialect: 'mysql'
 })
 
